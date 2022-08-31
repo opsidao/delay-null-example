d25fb6a0ac49c5d0ae61fd7467fb65a7559ae6f2
Juan González <juan.gonzalez@femtasy.com>
2022-08-31 13:35:36 +0200

    Add example

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,55 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  describe("with not parameters to setup", () => {
+    it('throws "Exceeded timeout of 5000 ms for a test.', async () => {
+      const callback = jest.fn();
+      const user = userEvent.setup();
+
+      render(<App callback={callback} />);
+
+      await user.click(screen.getByRole("button"));
+
+      expect(callback).toHaveBeenCalledWith(0);
+    });
+  });
+
+  describe("with advabceTimers set to jest.advanceTimersByTime", () => {
+    it("advances all timers too fast", async () => {
+      const callback = jest.fn();
+      const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+
+      render(<App callback={callback} />);
+
+      await user.click(screen.getByRole("button"));
+
+      expect(callback).toHaveBeenCalledWith(0);
+      expect(callback).not.toHaveBeenCalledWith(1);
+    });
+  });
+
+  describe("with delay set to null", () => {
+    it("allows to manage the timers on your own", async () => {
+      const callback = jest.fn();
+      const user = userEvent.setup({ delay: null });
+
+      render(<App callback={callback} />);
+
+      await user.click(screen.getByRole("button"));
+
+      expect(callback).toHaveBeenCalledWith(0);
+      expect(callback).not.toHaveBeenCalledWith(1);
+
+      jest.runAllTimers();
+
+      expect(callback).toHaveBeenCalledWith(1);
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IProps {
+  callback: (arg: number) => void;
+}
+
+function App({ callback }: IProps) {
+  const runCallback = React.useCallback(() => {
+    callback(0);
+
+    // We need to have this second call to happen in the next event loop.
+    setTimeout(() => callback(1), 0);
+  }, [callback]);
+
+  return <button onClick={runCallback} />;
 }
 
 export default App;
